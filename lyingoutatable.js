c39f52d2b533d59f6f494e0f829fa036611f48e4
AlejandrDiaz <alu0100761948@ull.edu.es>
2017-03-27 11:26:50 +0100

    añadiendo hecho paso 1

--- lyingoutatable.js
+++ lyingoutatable.js
@@ -1,3 +1,6 @@
+let UnderlinedCell = require('./underlinedcell.js');
+let RTextCell = require('./rttextcell.js');
+let TextCell = require('./textcell.js');
 // Utils: Monkey Patching
 String.prototype.repeat = function(times) {
   var result = "";
@@ -12,23 +15,25 @@ Array.prototype.range = function(block) {
     r.push(block(i));
   }
   return r;
-} 
+}
+
+
 // End Utils
 
 // TextCell Class
-function TextCell(text) {
+/*function TextCell(text) {
   this.text = text.split("\n");
 }
 /*
   minWidth() returns a number indicating this cell’s minimum width
   (in characters).
-*/
+
 TextCell.prototype.minWidth = function() {
-  /* 
+  /*
     The reduce() method applies a function against an accumulator and
     each element in the array (from left to right) to reduce it to a
     single value.
-  */
+
   return this.text.reduce(function(width, line) {
     return Math.max(width, line.length);
   }, 0);
@@ -36,7 +41,7 @@ TextCell.prototype.minWidth = function() {
 /*
   minHeight() returns a number indicating the minimum height this
   cell requires (in lines).
-*/
+
 TextCell.prototype.minHeight = function() {
   return this.text.length;
 };
@@ -44,7 +49,7 @@ TextCell.prototype.minHeight = function() {
   draw(width, height) returns an array of length height, which contains
   a series of strings that are each width characters wide. This
   represents the content of the cell.
-*/
+
 TextCell.prototype.draw = function(width, height) {
   var result = [];
   for (var i = 0; i < height; i++) {
@@ -52,7 +57,7 @@ TextCell.prototype.draw = function(width, height) {
     result.push(line + " ".repeat(width - line.length));
   }
   return result;
-};    
+};
 
 // End TextCell
 
@@ -63,28 +68,28 @@ function UnderlinedCell(inner) {
 /*
   minWidth() returns a number indicating this cell’s minimum width
   (in characters).
-*/
+
 UnderlinedCell.prototype.minWidth = function() {
   return this.inner.minWidth();
 };
-/*
+
   minHeight() returns a number indicating the minimum height this
   cell requires (in lines).
-*/
+
 UnderlinedCell.prototype.minHeight = function() {
   return this.inner.minHeight() + 1;
 };
 
-/*
+
   draw(width, height) returns an array of length height, which contains
   a series of strings that are each width characters wide. This
   represents the content of the cell.
-*/
+
 UnderlinedCell.prototype.draw = function(width, height) {
   return this.inner.draw(width, height - 1)
     .concat(["-".repeat(width)]);
-};    
-// End UnderlinedCell    
+};
+// End UnderlinedCell
 
 // RTextCell
 function RTextCell(text) {
@@ -99,7 +104,7 @@ RTextCell.prototype.draw = function(width, height) {
   }
   return result;
 };
-// End RTextCell    
+// End RTextCell
 
 //---------------------------------------------
 /*
@@ -123,7 +128,7 @@ function colWidths(rows) {
     }, 0);
   });
 }
-    
+
 function dataTable(data) {
   var keys = Object.keys(data[0]);
   var headers = keys.map(function(name) {
@@ -162,8 +167,8 @@ function drawTable(rows) {
   }
 
   return rows.map(drawRow).join("\n");
-}    
-    
+}
+
 function drawIt(data) {
   return drawTable(dataTable(data));
 }
@@ -175,6 +180,3 @@ module.exports = {
   RTextCell: RTextCell,
   UnderlinedCell: UnderlinedCell
 };
-
-    
-   
