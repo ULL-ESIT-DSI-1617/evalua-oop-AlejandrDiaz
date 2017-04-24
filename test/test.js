fb8880df434fa738a466dc9890c2edea66d39ed2
AlejandrDiaz <alu0100761948@ull.edu.es>
2017-04-24 17:37:43 +0100

    añadiendo pruebas travis

--- test/test.js
+++ test/test.js
@@ -1,6 +1,9 @@
 // Testing drawIt
 var dt = require("../lyingoutatable.js");
 var drawIt = dt.drawIt;
+var UnderlinedCell = dt.UnderlinedCell;
+var RTextCell = dt.RTextCell;
+require("should");
 
 var MOUNTAINS = [
   {name: "Kilimanjaro\nMontaña mágica", height: 5895, country: "Tanzania"},
@@ -12,16 +15,16 @@ var MOUNTAINS = [
   {name: "Popocatepetl", height: 5465, country: "Mexico"}
 ];
 
-var expected = 
-`name           height country      
+var expected =
+`name           height country
 -------------- ------ -------------
-Kilimanjaro      5895 Tanzania     
-Montaña mágica                     
-Everest          8848 Nepal        
-                      País lejano  
-Mount Fuji       3776 Japan        
-Mont Blanc       4808 Italy/France 
-Vaalserberg       323 Netherlands  
+Kilimanjaro      5895 Tanzania
+Montaña mágica
+Everest          8848 Nepal
+                      País lejano
+Mount Fuji       3776 Japan
+Mont Blanc       4808 Italy/France
+Vaalserberg       323 Netherlands
 Denali           6168 United States
 Popocatepetl     5465 Mexico       `;
 
@@ -46,14 +49,51 @@ function checkerboard() {
   return rows;
 }
 
-var expectedCheckerboard = 
+var expectedCheckerboard =
 `##    ##    ##
-   ##    ##   
+   ##    ##
 ##    ##    ##
-   ##    ##   
+   ##    ##
 ##    ##    ##`;
+
 describe("drawTable", function() {
   it("must draw the checkerboard correctly", function() {
     drawTable(checkerboard()).should.equal(expectedCheckerboard);
+  })
+});
+
+// Test TextCell
+
+var textcell = new TextCell(`Kilimanjaro\nMontaña Mágica`);
+var expectedtextcell = [ `Kilimanjaro   `, `Montaña Mágica` ];
+
+describe("TextCell", function() {
+  it("must draw the textcell correctly", function() {
+    (textcell.draw(14, 2)[0]).should.equal(expectedtextcell[0]);
+    (textcell.draw(14, 2)[1]).should.equal(expectedtextcell[1]);
+  })
+});
+
+// Test UnderlinedCell
+
+var undercell = new UnderlinedCell(new TextCell(`Everest`));
+var expectedunderlinedcell = [ `Everest       `, `--------------` ];
+
+describe("UnderlinedCell", function() {
+  it("must draw the underlinedcell correctly", function() {
+    (undercell.draw(14, 2)[0]).should.equal(expectedunderlinedcell[0]);
+    (undercell.draw(14, 2)[1]).should.equal(expectedunderlinedcell[1]);
+  })
+});
+
+//Test RTextCell
+
+var Rtextcell = new RTextCell(`8848`);
+
+var expectedRtextcell = [`          8848`];
+
+describe("RTextCell", function() {
+  it("must draw the rtextCell correctly", function() {
+    (Rtextcell.draw(14, 1)[0]).should.equal(expectedRtextcell[0]);
   })
 });
